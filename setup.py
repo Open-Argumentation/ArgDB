71b6a78a2f3c016bb8af63853890b5646c2c1d02
Simon Wells <siwells@gmail.com>
2020-06-29 23:10:14 +0100

    Be able to build a package from the off

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='argdb',
+    description='ArgDB - A Datastore for arguments ',
+    long_description=readme,
+    license=license,
+    author='Simon Wells',
+    url='https://github.com/Open-Argumentation/ArgDB',
+    author_email='mail@simonwells.org',
+    version='0.1',
+    packages=find_packages(exclude=('deploy', 'etc', 'examples'))
+)
