0c5ea146fd5449109151cf180c9b4757e1485aa2
Simon Wells <siwells@gmail.com>
2020-07-03 18:45:55 +0100

    Add initial functions to suit plugin api for couchdb support

---
+++ argdb/datastores/type_couchdb.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+import json
+import os
+import uuid
+import sadface as sf
+
+def add_datastore(db_name):
+    """
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: 
+
+    """
+    print("hello")
+#    pass
+
+def add_doc(db_name, doc):
+    """
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: None
+    """
+    pass
+
+def clear_datastore(db_name):
+    """
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: None
+    """
+    pass
+ 
+def delete_datastore(db_name):
+    """
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: None
+    """
+    pass
+
+
+def delete_doc(db_name, doc_id):
+    """
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: None
+    """
+    pass
+
+
+def get_datastore(db_name):
+    """
+    object.
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: 
+    """
+    return []
+
+
+def get_doc(db_name, doc_id):
+    """
+
+    This function is a requirement of the ArgDB plugin architecture
+
+    Returns: A SADFace document
+    """
+    pass
+
+def get_size(db_name):
+    """
+    """
+    return 666
+    
