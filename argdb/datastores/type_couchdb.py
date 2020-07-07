18ebe550124cd8241f6c1507457ed2d70209cefa
Simon Wells <siwells@gmail.com>
2020-07-07 21:12:48 +0100

    Support for adding a new CouchDB Datastrore

--- argdb/datastores/type_couchdb.py
+++ argdb/datastores/type_couchdb.py
@@ -3,7 +3,10 @@
 import json
 import os
 import uuid
+import requests as rq
 import sadface as sf
+
+from .. import config
 
 def add_datastore(db_name):
     """
@@ -12,8 +15,21 @@ def add_datastore(db_name):
     Returns: 
 
     """
-    print("hello")
-#    pass
+    db_type = config.current.get(db_name, "type")
+    db_ip   = config.current.get(db_name, "ip")
+    db_port = config.current.get(db_name, "port")
+    db_protocol = config.current.get(db_name, "protocol")
+    db_username = config.current.get(db_name, "username")
+    db_password = config.current.get(db_name, "password")
+
+    url = db_protocol + "://" + db_username + ":" + db_password + "@" + db_ip + ":" + db_port + "/" + db_name
+    ds = url + "/"
+    if not db_exists(ds):
+        try:
+            r = rq.put(url)
+            r.raise_for_status()           
+        except rq.exceptions.HTTPError as e:
+            print(e)
 
 def add_doc(db_name, doc):
     """
@@ -32,6 +48,18 @@ def clear_datastore(db_name):
     Returns: None
     """
     pass
+
+def db_exists(address):
+    """
+    Check whether a nominated DB exists
+
+    Returns: True if the nominated DB exists, False otherwise
+    """
+    r = rq.get(address)
+    if r.status_code == rq.codes.ok:
+        return True
+    else:
+        return False
  
 def delete_datastore(db_name):
     """
