33f256506c8043aedef878cfa6af7dad44958256
Simon Wells <siwells@gmail.com>
2020-07-09 18:56:56 +0100

    Refining the various UI modes

--- argdb/ui.py
+++ argdb/ui.py
@@ -3,7 +3,36 @@
 from . import argdb
 
 import argparse
-#import cmd
+import cmd
+
+class Shell(cmd.Cmd):
+    """
+    The ArgDB REPL
+    """
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '(ArgDB) '
+        self.intro = "The ArgDB REPL. Type 'help', '?', or 'help <command>' for assistance"
+
+    def do_quit(self, line):
+        """
+        Quit the SADRace REPL.
+        """
+        return True
+
+    def emptyline(self):
+        pass
+    
+    do_q = do_quit
+
+
+def cli(args):
+    """
+    Process arguments passed in from the command linke
+    """
+    pass
+    
 
 def web():
     """
@@ -16,8 +45,10 @@ def web():
 
 def main():
     parser = argparse.ArgumentParser(description="This is the ArgDB Python tool")
-    parser.add_argument("-i", "--interactive", help="Use the ArgDB REPL (WARNING EXPERIMENTAL)", action="store_true")
-    parser.add_argument("-w", "--web", help="Launch the Web UI", action="store_true")
+    parser.add_argument("-i", "--interactive", help="Use the ArgDB REPL (WARNING EXPERIMENTAL)", 
+        action="store_true")
+    parser.add_argument("-w", "--web", help="Launch the Web UI  (WARNING EXPERIMENTAL)", 
+        action="store_true")
 
     args = parser.parse_args()
 
@@ -27,6 +58,10 @@ def main():
         
     elif args.interactive:
         print("ArgDB Interactive REPL")
+        Shell().cmdloop()
+
     else:
         print("ArgDB CLI UI")
+        cli(args)
+        
         
