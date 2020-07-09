adc2e91584c1bc722f535ae1208b8d9f933eb335
Simon Wells <siwells@gmail.com>
2020-07-09 17:44:16 +0100

    Boilerplate for interactive REPL

--- argdb/ui.py
+++ argdb/ui.py
@@ -7,12 +7,14 @@ import cmd
 
 def main():
     parser = argparse.ArgumentParser(description="This is the ArgDB Python tool")
+    parser.add_argument("-i", "--interactive", help="Use the ArgDB REPL (WARNING EXPERIMENTAL)", action="store_true")
     parser.add_argument("-w", "--web", help="Launch the Web UI", action="store_true")
 
     args = parser.parse_args()
 
     if args.web:
         print("ArgDB Web UI")
-
+    elif args.interactive:
+        print("ArgDB Interactive REPL")
     else:
         print("ArgDB CLI UI")
