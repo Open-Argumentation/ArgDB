a53015fb28db4f1f34a739dc561931e29617f9a6
Simon Wells <siwells@gmail.com>
2023-09-08 15:02:25 +0100

    Minor cleanup of commented code

--- argdb/cli.py
+++ argdb/cli.py
@@ -33,33 +33,4 @@ def process(args):
         if doc is not None:
             print(doc)
         argdb.cleanup()
-
-        """
-        if args.add_datastore:
-            print("Adding the following datastore: "+str(args.add_datastore))
-            argdb.add_datastore(args.add_datastore)
-            exit(1)
-
-        if args.remove_datastore:
-            print()
-            db_name = args.remove_datastore
-            if argdb.delete_datastore(db_name):
-                print("Deleting the following datastore: " + db_name)
-            else:
-                print("There doesn't appear to be a datastore named: " + db_name)
-            print(argdb.info())
-            exit(1)
-
-        if args.datastore:
-            print("Using the following datastore: " + args.datastore)
-            if args.add_document:
-                print("Adding a sadface document to: "+ args.datastore)
-                argdb.add_doc(args.datastore, args.add_document)
-            elif args.remove_document:
-                print("Deleting a sadface document from: "+ args.datastore)
-                argdb.delete_doc(args.datastore, args.remove_document)
-            elif args.get_document:
-                doc = argdb.get_doc(args.datastore, args.get_document)
-                print(sadface.prettyprint(doc))
-       """  
             
